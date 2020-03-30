78aa5953d24d5640829a4b79a07da7ed5b35ee2f
subratgupta <asubratgupta@gmail.com>
2020-03-30 11:19:11 +0530

    add_course, search_learners & search_course feature added. Switching of windows is perfectly working. Windows Centered with PopUp UI fixed Clickable Links in Search Course tried to refresh learners results, but not worked properly.

--- iTeamApp.py
+++ iTeamApp.py
@@ -89,6 +89,7 @@ class HomePage(tk.Frame):
             row=4)
         button = tk.Button(root, text='Search Course', font=("Arial", 20), width=15, fg="blue", command=lambda: master.switch_frame(SearchCourse))
         button.grid(row=5)
+        tk.Label(root, text='\n\niTeam is the one, Together we learn', bg='#90CAF9',  padx=25, pady=10).grid(row=6)
 
 
 class AddCourse(tk.Frame):
@@ -229,27 +230,44 @@ class SearchLearners(tk.Frame):
         def change_dropdown(*args):
             return tkvar.get()
 
+        label_list = [['']*4]*10
+
         def update(*args):
             global course_url, language, learners_details
+            for label_row in label_list:
+                for label in label_row:
+                    if type('') is not type(label):
+                        label.destroy()
             learners_details = list()
             # print('clicked')
             course_url, language = e1.get(), change_dropdown()
             find_learners(course_url, language)
             results = ''
-            tk.Label(root, text='Search Results').grid(row=4, padx=10)
-            tk.Label(root, text='Full Name', font=LARGE_FONT, bg='#90CAF9', fg='#263238').grid(row=5, column=0, padx=5, pady=5)
-            tk.Label(root, text='Contact Number', font=LARGE_FONT, bg='#90CAF9', fg='#263238').grid(row=5, column=1, padx=5, pady=5)
-            tk.Label(root, text='Email ID', font=LARGE_FONT, bg='#90CAF9', fg='#263238').grid(row=5, column=2, padx=5, pady=5)
-            tk.Label(root, text='Language', font=LARGE_FONT, bg='#90CAF9', fg='#263238').grid(row=5, column=3, padx=5, pady=5)
+            label_list[0][0] = tk.Label(root, text='Search Results')
+            label_list[0][0].grid(row=4, padx=10)
+            label_list[1][0] = tk.Label(root, text='Full Name', font=LARGE_FONT, bg='#90CAF9', fg='#263238')
+            label_list[1][0].grid(row=5, column=0, padx=5, pady=5)
+            label_list[1][1] = tk.Label(root, text='Contact Number', font=LARGE_FONT, bg='#90CAF9', fg='#263238')
+            label_list[1][1].grid(row=5, column=1, padx=5, pady=5)
+            label_list[1][2] = tk.Label(root, text='Email ID', font=LARGE_FONT, bg='#90CAF9', fg='#263238')
+            label_list[1][2].grid(row=5, column=2, padx=5, pady=5)
+            label_list[1][3] = tk.Label(root, text='Language', font=LARGE_FONT, bg='#90CAF9', fg='#263238')
+            label_list[1][3].grid(row=5, column=3, padx=5, pady=5)
             for idx, learner in enumerate(learners_details):
+                if idx>7:
+                    break
                 # results = results + '\n' + learner['full_name'] + '\t' + learner['contact_number'] + '\t' + learner['email_id'] + '\t' + learner['language']
                 # print(results)
                 # tk.Label(root, text=learner['full_name'].ljust(10)  + '\t\t' + learner['contact_number'].ljust(10)  + '\t\t' + learner[
                 #     'email_id'].ljust(10) + '\t\t' + learner['language'].ljust(10) ).grid(row=6+idx, padx=5, pady=5)
-                tk.Label(root, text=learner['full_name'], bg='#90CAF9', fg='#1A237E').grid(row=6+idx, column=0, padx=5, pady=5)
-                tk.Label(root, text=learner['contact_number'], bg='#90CAF9', fg='#1A237E').grid(row=6+idx, column=1, padx=5, pady=5)
-                tk.Label(root, text=learner['email_id'], bg='#90CAF9', fg='#1A237E').grid(row=6+idx, column=2, padx=5, pady=5)
-                tk.Label(root, text=learner['language'], bg='#90CAF9', fg='#1A237E').grid(row=6+idx, column=3, padx=5, pady=5)
+                label_list[2+idx][0] = tk.Label(root, text=learner['full_name'], bg='#90CAF9', fg='#1A237E')
+                label_list[2+idx][0].grid(row=6+idx, column=0, padx=5, pady=5)
+                label_list[2+idx][1] = tk.Label(root, text=learner['contact_number'], bg='#90CAF9', fg='#1A237E')
+                label_list[2+idx][1].grid(row=6+idx, column=1, padx=5, pady=5)
+                label_list[2+idx][2] = tk.Label(root, text=learner['email_id'], bg='#90CAF9', fg='#1A237E')
+                label_list[2+idx][2].grid(row=6+idx, column=2, padx=5, pady=5)
+                label_list[2+idx][3] = tk.Label(root, text=learner['language'], bg='#90CAF9', fg='#1A237E')
+                label_list[2+idx][3].grid(row=6+idx, column=3, padx=5, pady=5)
 
         tk.Label(root, text='Course URL', font=LARGE_FONT, bg='#90CAF9',  padx=25, pady=10).grid(row=0)
         v = tk.StringVar(root, value=clicked_url)
