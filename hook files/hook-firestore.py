728ce76a93c8e2a4b364d28e20d492fd79af0896
subratgupta <asubratgupta@gmail.com>
2020-03-31 20:42:29 +0530

    add_course, search_learners & search_course feature added. Switching of windows is perfectly working. Windows Centered with PopUp UI fixed Clickable Links in Search Course Refresh Button Added (Trying to make exe) Refresh after search course problem fixed exe file attached config file for auto-py-to-exe attached hook files attached

---
+++ hook files/hook-firestore.py
@@ -0,0 +1,13 @@
+from PyInstaller.utils.hooks import copy_metadata
+
+
+
+
+
+datas = copy_metadata('google-cloud-core')
+
+
+datas += copy_metadata('google-cloud-firestore')
+
+
+datas += copy_metadata('google-api-core')
